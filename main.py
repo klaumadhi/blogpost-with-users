b5ab02fd7f312d920ab3967ca3f3b73bab783d6d
Klaurent Madhi <120852517+klaumadhi@users.noreply.github.com>
2023-06-11 00:42:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
-server = app.server
 app.app_context().push()
 
 login_manager = LoginManager()
